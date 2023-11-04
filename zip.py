ce182752e58229c4935313eb10c13180360bca2e
uzmaa1 <68188098+uzmaa1@users.noreply.github.com>
2023-11-05 04:44:20 +0500

    Create zip.py

---
+++ zip.py
@@ -0,0 +1,12 @@
+
+#List1
+Name = ['tomy', 'krish', 'nickol', 'juli']
+ 
+# List2
+marks = [75, 80, 56, 62]
+
+#Above two lists can be merged by using list(zip()) 
+list_of_tuples = list(zip(Name, marks))
+ 
+# Assign data to tuples.
+list_of_tuples 

820ce90d9c89c1c97255b4293fbbfee8d70f38e2
uzmaa1 <68188098+uzmaa1@users.noreply.github.com>
2023-11-05 04:55:05 +0500

    Update dataframe_4rm_zip.py

--- dataframe_4rm_zip.py
+++ dataframe_4rm_zip.py
@@ -1,7 +1,12 @@
 
 # Converting lists of tuples into
 # pandas Dataframe.
-
+#List1
+Name = ['tomy', 'krish', 'nickol', 'juli']
+ 
+# List2
+marks = [75, 80, 56, 62]
+list_of_tuples = list(zip(Name, marks))
 df = pd.DataFrame(list_of_tuples, columns = ['Name', 'marks'])
 
   

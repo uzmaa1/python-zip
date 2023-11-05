d4b5deb0a6d203e1d6e280c60329927b2763fdb3
uzmaa1 <68188098+uzmaa1@users.noreply.github.com>
2023-11-05 05:17:17 +0500

    Update and rename tuples2List_by_zip.py to mergeLists2tuples_by_zip.py

--- mergeLists2tuples_by_zip.py
+++ mergeLists2tuples_by_zip.py
@@ -7,5 +7,4 @@ marks = [75, 80, 56, 62]
 #Above two lists can be merged by using list(zip()).
 tuples = list(zip(Name, marks))
 
-result = [item for t in tuples for item in t]
-print("Convert list of tuples into list: ",result)
+print("Convert 2 lists into tuple: ",result)
